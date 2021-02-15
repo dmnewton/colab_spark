1729ca864f4107f801603509e4db714b90b1865d
David Newton <dnewton@ford.com>
2021-02-15 18:52:53 +0100

    java 8

--- colab_spark/__init__.py
+++ colab_spark/__init__.py
@@ -20,11 +20,14 @@ def install_all():
         print(rurl)
 
     if not(os.path.exists(spark_home)):
+        os.system("apt-get install openjdk-8-jdk-headless -qq > /dev/null")
+        os.system("update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java")
         os.system('wget -q https://downloads.apache.org/spark/spark-3.0.1/spark-3.0.1-bin-hadoop3.2.tgz')
         os.system('tar xf spark-3.0.1-bin-hadoop3.2.tgz')
         os.system('rm spark-3.0.1-bin-hadoop3.2.tgz')
 
-    os.environ["JAVA_HOME"] = "/usr/lib/jvm/java-11-openjdk-amd64"
+    #os.environ["JAVA_HOME"] = "/usr/lib/jvm/java-11-openjdk-amd64"
+    os.environ["JAVA_HOME"] = "/usr/lib/jvm/java-8-openjdk-amd64"
     os.environ["SPARK_HOME"] = spark_home
     os.environ["SPARK_LOCAL_IP"] = "127.0.0.1"
 
