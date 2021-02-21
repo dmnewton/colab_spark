a00280f15cee72b58190383b98eab76562489344
David Newton <dnewton@ford.com>
2021-02-21 20:50:37 +0100

    new spark version

--- colab_spark/__init__.py
+++ colab_spark/__init__.py
@@ -5,10 +5,10 @@ import psutil
 in_colab=True
 try:
     from google.colab.output import eval_js
-    spark_home='/content/spark-3.0.1-bin-hadoop3.2'
+    spark_home='/content/spark-3.0.2-bin-hadoop3.2'
 except:
     in_colab=False
-    spark_home='spark-3.0.1-bin-hadoop3.2'
+    spark_home='spark-3.0.2-bin-hadoop3.2'
 
 def install_all():
 
@@ -18,13 +18,13 @@ def install_all():
         rurl=eval_js("google.colab.kernel.proxyPort(9090)")
         print("url for Spark UI")
         print(rurl)
-
-    if not(os.path.exists(spark_home)):
         os.system("apt-get install openjdk-8-jdk-headless -qq > /dev/null")
         os.system("update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java")
-        os.system('wget -q https://downloads.apache.org/spark/spark-3.0.1/spark-3.0.1-bin-hadoop3.2.tgz')
-        os.system('tar xf spark-3.0.1-bin-hadoop3.2.tgz')
-        os.system('rm spark-3.0.1-bin-hadoop3.2.tgz')
+
+    if not(os.path.exists(spark_home)):
+        os.system('wget -q https://downloads.apache.org/spark/spark-3.0.2/spark-3.0.2-bin-hadoop3.2.tgz')
+        os.system('tar xf spark-3.0.2-bin-hadoop3.2.tgz')
+        os.system('rm spark-3.0.2-bin-hadoop3.2.tgz')
 
     #os.environ["JAVA_HOME"] = "/usr/lib/jvm/java-11-openjdk-amd64"
     os.environ["JAVA_HOME"] = "/usr/lib/jvm/java-8-openjdk-amd64"
@@ -46,7 +46,7 @@ def install_all():
                 pass
         except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
             pass
-    
+
     if start_proxy:
         cmd = "nohup python3 {}/ui_proxy.py &".format(os.path.dirname(__file__))
         print(cmd)
